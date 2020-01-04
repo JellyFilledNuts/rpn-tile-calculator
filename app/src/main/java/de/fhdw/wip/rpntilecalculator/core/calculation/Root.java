7bd90d4fc3c3221e64064d734537b3ae77d98ec4
Getuart Istogu <Getuart.Istogu@users.noreply.github.com>
2020-01-04 18:45:42 +0100

    Power und Root hinzugefügt OFraction: OFraction <-> ODouble

---
+++ app/src/main/java/de/fhdw/wip/rpntilecalculator/core/calculation/Root.java
@@ -0,0 +1,57 @@
+package de.fhdw.wip.rpntilecalculator.core.calculation;
+
+import de.fhdw.wip.rpntilecalculator.core.model.operand.ODouble;
+import de.fhdw.wip.rpntilecalculator.core.model.operand.OFraction;
+import de.fhdw.wip.rpntilecalculator.core.model.operand.OMatrix;
+import de.fhdw.wip.rpntilecalculator.core.model.operand.Operand;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Contract;
+
+public class Root extends Action{
+    @NotNull private static final Root ROOT = new Root();
+    @NotNull private static final Power POWER = Power.getInstance();
+
+    @Contract(pure = true) @NotNull public static Root getInstance() { return ROOT; }
+    private Root() { }
+
+    @NotNull @Override
+    public Operand with(@NotNull Operand... operands) throws CalculationException {
+        positionDoesNotMatter = true;
+        scopedAction = this;
+        return super.with(operands);
+    }
+
+    //region Double
+    //------------------------------------------------------------------------------------
+
+    @Contract(pure = true) @NotNull ODouble on(@NotNull ODouble radicand, @NotNull ODouble exponent) {
+        return POWER.on(radicand, new ODouble(1/exponent.getDouble()));
+    }
+
+    @Contract(pure = true) @NotNull ODouble on(@NotNull ODouble radicand, @NotNull OFraction exponent){
+        return POWER.on(radicand, new OFraction(1/exponent.getDouble()));
+    }
+
+    //region Fraction
+    //------------------------------------------------------------------------------------
+    @Contract(pure = true) @NotNull OFraction on(@NotNull OFraction radicand, @NotNull ODouble exponent){
+        return POWER.on(radicand, new OFraction(1/exponent.getDouble()));
+    }
+
+    @Contract(pure = true) @NotNull OFraction on(@NotNull OFraction radicand, @NotNull OFraction exponent){
+        return POWER.on(radicand, new ODouble(1/exponent.getDouble()));
+    }
+
+    //region Matrix
+    //------------------------------------------------------------------------------------
+    @Contract(pure = true) @NotNull OMatrix on(@NotNull OMatrix radicand, @NotNull ODouble exponent) throws CalculationException{
+        return POWER.on(radicand, new ODouble(1/exponent.getDouble()));
+    }
+
+    @Contract(pure = true) @NotNull OMatrix on(@NotNull OMatrix radicand, @NotNull OFraction exponent) throws CalculationException{
+        return POWER.on(radicand, new ODouble(1/exponent.getDouble()));
+    }
+
+
+}
