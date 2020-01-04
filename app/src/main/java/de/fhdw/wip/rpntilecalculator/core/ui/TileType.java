a686c8105b47dd4c3a809b31944f34a9c5a29d41
Khang Pham <bfwi317bph>
2020-01-04 18:52:55 +0100

    added one line (slash)

--- app/src/main/java/de/fhdw/wip/rpntilecalculator/core/ui/TileType.java
+++ app/src/main/java/de/fhdw/wip/rpntilecalculator/core/ui/TileType.java
@@ -3,12 +3,14 @@ package de.fhdw.wip.rpntilecalculator.core.ui;
 import de.fhdw.wip.rpntilecalculator.core.calculation.Action;
 import de.fhdw.wip.rpntilecalculator.core.calculation.Minus;
 import de.fhdw.wip.rpntilecalculator.core.calculation.Plus;
+import de.fhdw.wip.rpntilecalculator.core.calculation.Slash;
 
 public enum TileType {
 
     STACK("", null),
     O_MINUS("-", Minus.getInstance()),
-    O_PLUS("+", Plus.getInstance());
+    O_PLUS("+", Plus.getInstance()),
+    O_SLASH("/", Slash.getInstance());
 
     private String text;
     private Action action;
