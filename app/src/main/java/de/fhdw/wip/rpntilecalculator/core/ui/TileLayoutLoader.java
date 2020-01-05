5ac9ed751f7724413e57b567207827c4bf30ef9e
Tom Bockhorn <bockhorn.tom@web.de>
2020-01-05 22:20:18 +0100

    Layout Loading via new format enabled. LayoutLoader Design started.

---
+++ app/src/main/java/de/fhdw/wip/rpntilecalculator/core/ui/TileLayoutLoader.java
@@ -0,0 +1,17 @@
+package de.fhdw.wip.rpntilecalculator.core.ui;
+
+import org.jetbrains.annotations.NotNull;
+
+public class TileLayoutLoader {
+
+    private final static String VALUE_SEPERATOR = ";";
+    private final static String COLUMN_SEPERATOR = ";;";
+    private final static String ROW_SEPERATOR = ";;;";
+
+    @NotNull
+    public TileLayout loadLayout(String indicator) {
+        return null;
+    }
+
+
+}
--- app/src/main/java/de/fhdw/wip/rpntilecalculator/core/ui/TileScheme.java
+++ app/src/main/java/de/fhdw/wip/rpntilecalculator/core/ui/TileScheme.java
@@ -1,28 +1,38 @@
 package de.fhdw.wip.rpntilecalculator.core.ui;
 
+/*
+ * Summary: TileScheme contains the information about tiles and is used to create tiles and save them
+ * Author:  Tom Bockhorn
+ */
+
+
+import org.jetbrains.annotations.NotNull;
+
 public class TileScheme {
 
-    private TileType type;
-    private String content;
+    @NotNull private TileType type;
+    @NotNull private String content;
 
-    public TileScheme(TileType type, String content) {
+    TileScheme(@NotNull TileType type, @NotNull String content) {
         this.type = type;
         this.content = content;
     }
 
+    @NotNull
     public TileType getType() {
         return type;
     }
 
-    public void setType(TileType type) {
+    public void setType(@NotNull TileType type) {
         this.type = type;
     }
 
+    @NotNull
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent(@NotNull String content) {
         this.content = content;
     }
 }
