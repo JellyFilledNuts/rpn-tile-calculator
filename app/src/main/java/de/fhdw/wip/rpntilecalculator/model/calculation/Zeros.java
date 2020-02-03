a1feb561855ae0446a19f89d13cfb3d535a68e1e
Getuart Istogu <Getuart.Istogu@users.noreply.github.com>
2020-02-03 11:49:56 +0100

    Changed the return type from double[] to ODouble[] in Zeros 'on'

--- app/src/main/java/de/fhdw/wip/rpntilecalculator/model/calculation/Zeros.java
+++ app/src/main/java/de/fhdw/wip/rpntilecalculator/model/calculation/Zeros.java
@@ -3,6 +3,7 @@ package de.fhdw.wip.rpntilecalculator.model.calculation;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import de.fhdw.wip.rpntilecalculator.model.operands.ODouble;
 import de.fhdw.wip.rpntilecalculator.model.operands.OPolynom;
 import de.fhdw.wip.rpntilecalculator.model.operands.Operand;
 
@@ -16,7 +17,7 @@ public class Zeros extends Action {
     @NotNull public static final Zeros ZEROS = new Zeros();
 
     @Contract(pure = true) @NotNull public static Zeros getInstance() { return ZEROS; }
-    public Zeros() { }
+    public Zeros() {requiredNumOfOperands = new int[] {1}; }
 
     @NotNull @Override
     public Operand with(@NotNull Operand... operands) throws CalculationException {
@@ -24,8 +25,16 @@ public class Zeros extends Action {
         return super.with(operands);
     }
 
-    @Contract(pure = true) @NotNull double[] on(@NotNull OPolynom oPolynom) {
-        return calculateZeros(oPolynom);
+    @Contract(pure = true) @NotNull ODouble[] on(@NotNull OPolynom oPolynom) {
+        double[] results = calculateZeros(oPolynom);
+        int numberOfResults = results.length;
+        ODouble[] oResults = new ODouble[numberOfResults];
+        for(int i=0; i<numberOfResults; i++)
+        {
+            oResults[i] = new ODouble(results[i]);
+        }
+
+        return oResults;
     }
 
     // Function that calculates the zeros when called
