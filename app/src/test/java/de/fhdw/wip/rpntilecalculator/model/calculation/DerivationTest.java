3f84bf129cefb8da46186251407eaf8fba05c3e3
jlk98 <43638616+jlk98@users.noreply.github.com>
2020-02-04 21:43:46 +0100

    Create DerivationTest.java
    new unit test for derivation

---
+++ app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/DerivationTest.java
@@ -0,0 +1,59 @@
+package de.fhdw.wip.rpntilecalculator.model.calculation;
+
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
+import org.junit.Test;
+
+import de.fhdw.wip.rpntilecalculator.model.operands.OPolynom;
+import de.fhdw.wip.rpntilecalculator.model.operands.OTuple;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DerivationTest {
+    private Derivation DERIVATION = Derivation.getInstance();
+
+    @Test
+    public void derivation_isCorrect1() {
+        double[] functionValues = new double[] {2, 4, 6};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        double[] resultValues = new double[] {4, 4};
+        OPolynom derivation = new OPolynom(new PolynomialFunction(resultValues));
+        assertEquals(DERIVATION.on(function), derivation);
+    }
+
+    @Test
+    public void derivation_isCorrect2() {
+        double[] functionValues = new double[] {7, 9};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        double[] resultValues = new double[] {7};
+        OPolynom derivation = new OPolynom(new PolynomialFunction(resultValues));
+        assertEquals(DERIVATION.on(function), derivation);
+    }
+
+    @Test
+    public void derivation_isCorrect3() {
+        double[] functionValues = new double[] {12, -3, 12};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        double[] resultValues = new double[] {24, -3};
+        OPolynom derivation = new OPolynom(new PolynomialFunction(resultValues));
+        assertEquals(DERIVATION.on(function), derivation);
+    }
+
+    @Test
+    public void derivation_isCorrect4() {
+        double[] functionValues = new double[] {-3, 4, 12};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        double[] resultValues = new double[] {-6, 4};
+        OPolynom derivation = new OPolynom(new PolynomialFunction(resultValues));
+        assertEquals(DERIVATION.on(function), derivation);
+    }
+
+    @Test
+    public void derivation_isCorrect5() {
+        double[] functionValues = new double[] {1.5, 2, 7};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        double[] resultValues = new double[] {2,25, 2};
+        OPolynom derivation = new OPolynom(new PolynomialFunction(resultValues));
+        assertEquals(DERIVATION.on(function), derivation);
+    }
+}
