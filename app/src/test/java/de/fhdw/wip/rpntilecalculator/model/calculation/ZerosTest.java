b0a0233da3e410a0c021d46c804719dae3f3ecf1
jlk98 <43638616+jlk98@users.noreply.github.com>
2020-02-04 21:47:40 +0100

    Create ZerosTest.java
    unit test for Zeros

---
+++ app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/ZerosTest.java
@@ -0,0 +1,43 @@
+package de.fhdw.wip.rpntilecalculator.model.calculation;
+
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
+import org.junit.Test;
+import de.fhdw.wip.rpntilecalculator.model.operands.ODouble;
+import de.fhdw.wip.rpntilecalculator.model.operands.OPolynom;
+import de.fhdw.wip.rpntilecalculator.model.operands.OTuple;
+
+import static org.junit.Assert.assertTrue;
+
+public class ZerosTest {
+    private Zeros ZEROS = Zeros.getInstance();
+
+    @Test
+    public void zeros_isCorrect1() {
+        double[] functionValues = new double[] {2, 4};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        OTuple result = new OTuple(2, 0);
+        System.out.println(ZEROS.on(function));
+        assertTrue(ZEROS.on(new OPolynom(new PolynomialFunction(functionValues)))
+                .equalsValue(result));
+    }
+
+    @Test
+    public void zeros_isCorrect2() {
+        double[] functionValues = new double[] {3, 4, 6};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        OTuple result = new OTuple();
+        System.out.println(ZEROS.on(function));
+        assertTrue(ZEROS.on(new OPolynom(new PolynomialFunction(functionValues)))
+                .equalsValue(result));
+    }
+
+    @Test
+    public void zeros_isCorrect3() {
+        double[] functionValues = new double[] {1, 4, -4};
+        OPolynom function = new OPolynom(new PolynomialFunction(functionValues));
+        OTuple result = new OTuple(-4.828, 0.828);
+        System.out.println(ZEROS.on(function));
+        assertTrue(ZEROS.on(new OPolynom(new PolynomialFunction(functionValues)))
+                .equalsValue(result));
+    }
+}
