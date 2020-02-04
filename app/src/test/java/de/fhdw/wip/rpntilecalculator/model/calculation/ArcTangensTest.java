e986ad5e3ddae87951240eef73535f1319c85ae2
jlk98 <43638616+jlk98@users.noreply.github.com>
2020-02-04 21:41:55 +0100

    Update ArcTangensTest.java
    update

--- app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/ArcTangensTest.java
+++ app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/ArcTangensTest.java
@@ -10,11 +10,29 @@ public class ArcTangensTest {
     private ArcTangens ARC_TANGENS = ArcTangens.getInstance();
 
     @Test
-    public void arcTangensAngle_isCorrect() {
+    public void arcTangensAngle_isCorrect1() {
         System.out.println(ARC_TANGENS.on(
                 new ODouble(10)));
         assertTrue(ARC_TANGENS.on(
                 new ODouble(10)).equalsValue(new ODouble(0.1727924348551592))
+        );
+    }
+
+    @Test
+    public void arcTangensAngle_isCorrect2() {
+        System.out.println(ARC_TANGENS.on(
+                new ODouble(45)));
+        assertTrue(ARC_TANGENS.on(
+                new ODouble(45)).equalsValue(new ODouble(0.6657737500283538))
+        );
+    }
+
+    @Test
+    public void arcTangensAngle_isCorrect3() {
+        System.out.println(ARC_TANGENS.on(
+                new ODouble(-45)));
+        assertTrue(ARC_TANGENS.on(
+                new ODouble(-45)).equalsValue(new ODouble(-0.6657737500283538))
         );
     }
 }
