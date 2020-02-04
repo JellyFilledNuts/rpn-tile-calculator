b9d7ab46b7c925549d7fa2276274972caa5f2518
jlk98 <43638616+jlk98@users.noreply.github.com>
2020-02-04 21:46:58 +0100

    Update TangensTest.java
    update

--- app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/TangensTest.java
+++ app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/TangensTest.java
@@ -10,11 +10,29 @@ public class TangensTest {
     private Tangens TANGENS = Tangens.getInstance();
 
     @Test
-    public void tangensAngle_isCorrect() {
+    public void tangensAngle_isCorrect1() {
         System.out.println(TANGENS.on(
                 new ODouble(10)));
         assertTrue(TANGENS.on(
                 new ODouble(10)).equalsValue(new ODouble(0.17632698070846498))
+        );
+    }
+
+    @Test
+    public void tangensAngle_isCorrect2() {
+        System.out.println(TANGENS.on(
+                new ODouble(45)));
+        assertTrue(TANGENS.on(
+                new ODouble(45)).equalsValue(new ODouble(0.9999999999999999))
+        );
+    }
+
+    @Test
+    public void tangensAngle_isCorrect3() {
+        System.out.println(TANGENS.on(
+                new ODouble(-45)));
+        assertTrue(TANGENS.on(
+                new ODouble(-45)).equalsValue(new ODouble(-0.9999999999999999))
         );
     }
 }
