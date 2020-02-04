2c01c1efc0644821756c1b3df83596541c87a923
jlk98 <43638616+jlk98@users.noreply.github.com>
2020-02-04 21:41:12 +0100

    Update ArcSinusTest.java
    update

--- app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/ArcSinusTest.java
+++ app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/ArcSinusTest.java
@@ -10,12 +10,29 @@ public class ArcSinusTest {
     private ArcSinus ARC_SINUS = ArcSinus.getInstance();
 
     @Test
-    public void arcSinusAngle_isCorrect() {
+    public void arcSinusAngle_isCorrect1() {
         System.out.println(ARC_SINUS.on(
                 new ODouble(10)));
         assertTrue(ARC_SINUS.on(
                 new ODouble(10)).equalsValue(new ODouble(0.17543139267904395))
         );
     }
-}
 
+    @Test
+    public void arcSinusAngle_isCorrect2() {
+        System.out.println(ARC_SINUS.on(
+                new ODouble(45)));
+        assertTrue(ARC_SINUS.on(
+                new ODouble(45)).equalsValue(new ODouble(0.9033391107665127))
+        );
+    }
+
+    @Test
+    public void arcSinusAngle_isCorrect3() {
+        System.out.println(ARC_SINUS.on(
+                new ODouble(-45)));
+        assertTrue(ARC_SINUS.on(
+                new ODouble(-45)).equalsValue(new ODouble(-0.9033391107665127))
+        );
+    }
+}
