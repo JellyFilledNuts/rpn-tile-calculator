590d03c4f51aa0bd18300771e4a4b196dffe467c
jlk98 <43638616+jlk98@users.noreply.github.com>
2020-02-05 08:42:55 +0100

    Update ArcCosinusTest.java
    update comment

--- app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/ArcCosinusTest.java
+++ app/src/test/java/de/fhdw/wip/rpntilecalculator/model/calculation/ArcCosinusTest.java
@@ -6,6 +6,12 @@ import de.fhdw.wip.rpntilecalculator.model.operands.ODouble;
 
 import static org.junit.Assert.assertTrue;
 
+/*
+ * Summary: Unit test for the arc cosinus function
+ * Author:  Jannis Luca Keienburg
+ * Date:    2020/01/22
+ */
+
 public class ArcCosinusTest {
     private ArcCosinus ARC_COSINUS = ArcCosinus.getInstance();
 
